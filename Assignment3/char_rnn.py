2ad3d2f030e3207460c87a5e384008549bd7fae1
haithienld@gmail.com <haithienld@gmail.com>
2019-11-12 14:50:49 +0900

    update

--- Assignment3/char_rnn.py
+++ Assignment3/char_rnn.py
@@ -33,61 +33,65 @@ class Model():
                                           input_keep_prob=args.input_keep_prob,
                                           output_keep_prob=args.output_keep_prob)
             cells.append(cell)
-        # self.cell = cell = rnn.MultiRNNCell(cells)
-        self.cell = cell = rnn.MultiRNNCell([cell] * args.num_layers, state_is_tuple=True)
+        self.cell = cell = tf.nn.rnn_cell.MultiRNNCell(cells, state_is_tuple=True)
 
-        self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
-        self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        # input/target data (int32 since input is char-level)
+        self.input_data = tf.placeholder(
+            tf.int32, [args.batch_size, args.seq_length])
+        self.targets = tf.placeholder(
+            tf.int32, [args.batch_size, args.seq_length])
         self.initial_state = cell.zero_state(args.batch_size, tf.float32)
-        self.batch_pointer = tf.Variable(0, name="batch_pointer", trainable=False, dtype=tf.int32)
-        self.inc_batch_pointer_op = tf.assign(self.batch_pointer, self.batch_pointer + 1)
-        self.epoch_pointer = tf.Variable(0, name="epoch_pointer", trainable=False)
-        self.batch_time = tf.Variable(0.0, name="batch_time", trainable=False)
-        tf.summary.scalar("time_batch", self.batch_time)
 
-        def variable_summaries(var):
-            """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
-            with tf.name_scope('summaries'):
-                mean = tf.reduce_mean(var)
-                tf.summary.scalar('mean', mean)
-                #with tf.name_scope('stddev'):
-                #   stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-                #tf.summary.scalar('stddev', stddev)
-                tf.summary.scalar('max', tf.reduce_max(var))
-                tf.summary.scalar('min', tf.reduce_min(var))
-                #tf.summary.histogram('histogram', var)
-
+        # softmax output layer, use softmax to classify
         with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
-            variable_summaries(softmax_w)
+            softmax_w = tf.get_variable("softmax_w",
+                                        [args.rnn_size, args.vocab_size])
             softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
-            variable_summaries(softmax_b)
-            with tf.device("/cpu:0"):
-                embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-                inputs = tf.split(tf.nn.embedding_lookup(embedding, self.input_data), args.seq_length, 1)
-                inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
+        # transform input to embedding
+        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+
+        # dropout beta testing: double check which one should affect next line
+        if training and args.output_keep_prob:
+            inputs = tf.nn.dropout(inputs, args.output_keep_prob)
+
+        # unstack the input to fits in rnn model
+        inputs = tf.split(inputs, args.seq_length, 1)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+
+        # loop function for rnn_decoder, which take the previous i-th cell's output and generate the (i+1)-th cell's input
         def loop(prev, _):
             prev = tf.matmul(prev, softmax_w) + softmax_b
             prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
             return tf.nn.embedding_lookup(embedding, prev_symbol)
 
+        # rnn_decoder to generate the ouputs and final state. When we are not training the model, we use the loop function.
         outputs, last_state = legacy_seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if not training else None, scope='rnnlm')
         output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
+
+        # output layer
         self.logits = tf.matmul(output, softmax_w) + softmax_b
         self.probs = tf.nn.softmax(self.logits)
-        loss = legacy_seq2seq.sequence_loss_by_example([self.logits],
+
+        # loss is calculate by the log loss and taking the average.
+        loss = legacy_seq2seq.sequence_loss_by_example(
+                [self.logits],
                 [tf.reshape(self.targets, [-1])],
-                [tf.ones([args.batch_size * args.seq_length])],
-                args.vocab_size)
-        self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
-        tf.summary.scalar("cost", self.cost)
+                [tf.ones([args.batch_size * args.seq_length])])
+        with tf.name_scope('cost'):
+            self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
         self.final_state = last_state
         self.lr = tf.Variable(0.0, trainable=False)
         tvars = tf.trainable_variables()
+
+        # calculate gradients
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
                 args.grad_clip)
-        optimizer = tf.train.AdamOptimizer(args.learning_rate)
+        with tf.name_scope('optimizer'):
+            optimizer = tf.train.AdamOptimizer(self.lr)
+
+        # apply gradient change to the all the trainable variable.
         self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
         # instrument tensorboard
